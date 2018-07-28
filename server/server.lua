76acce7247c29c21e9baf51f354292a9615db58f
Unknown <Tanoshi.shinto@gmail.com>
2018-07-27 23:53:26 -0400

    Removed broke stuff that i missed becaue it WAS working and decided to break now.

--- server/server.lua
+++ server/server.lua
@@ -69,56 +69,6 @@ RegisterServerEvent("LSC:finished")
 AddEventHandler("LSC:finished", function(veh)
 	local model = veh.model --Display name from vehicle model(comet2, entityxf)
 	local mods = veh.mods
-	mods[0].mod - spoiler
-	mods[1].mod - front bumper
-	mods[2].mod - rearbumper
-	mods[3].mod - skirts
-	mods[4].mod - exhaust
-	mods[5].mod - roll cage
-	mods[6].mod - grille
-	mods[7].mod - hood
-	mods[8].mod - fenders
-	mods[10].mod - roof
-	mods[11].mod - engine
-	mods[12].mod - brakes
-	mods[13].mod - transmission
-	mods[14].mod - horn
-	mods[15].mod - suspension
-	mods[16].mod - armor
-	mods[23].mod - tires
-	mods[23].variation - custom tires
-	mods[24].mod - tires(Just for bikes, 23:front wheel 24:back wheel)
-	mods[24].variation - custom tires(Just for bikes, 23:front wheel 24:back wheel)
-	mods[25].mod - plate holder
-	mods[26].mod - vanity plates
-	mods[27].mod - trim design
-	mods[28].mod - ornaments
-	mods[29].mod - dashboard
-	mods[30].mod - dial design
-	mods[31].mod - doors
-	mods[32].mod - seats
-	mods[33].mod - steering wheels
-	mods[34].mod - shift leavers
-	mods[35].mod - plaques
-	mods[36].mod - speakers
-	mods[37].mod - trunk
-	mods[38].mod - hydraulics
-	mods[39].mod - engine block
-	mods[40].mod - cam cover
-	mods[41].mod - strut brace
-	mods[42].mod - arch cover
-	mods[43].mod - aerials
-	mods[44].mod - roof scoops
-	mods[45].mod - tank
-	mods[46].mod - doors
-	mods[48].mod - liveries
-	
-	--Toggle mods
-	mods[20].mod - tyre smoke
-	mods[22].mod - headlights
-	mods[18].mod - turbo
-	
-	--]]
 	local color = veh.color
 	local extracolor = veh.extracolor
 	local neoncolor = veh.neoncolor
